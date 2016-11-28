63e1b9d59387efb1441eef0f0dd11575b7bf4427
James Swift <james.rt.swift@gmail.com>
2016-11-28 18:45:48 +0000

    Reworked main files

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,27 +1,15 @@
+--[[--------------------------------------------------------------------------------------
+ _______                     ______                      _____ _____                
+ ___    |___________________ ___  /_____ ______________ ___  /____(_)______ _______ 
+ __  /| |_  ___/_  ___/_  _ \__  / _  _ \__  ___/_  __ `/_  __/__  / _  __ \__  __ \
+ _  ___ |/ /__  / /__  /  __/_  /  /  __/_  /    / /_/ / / /_  _  /  / /_/ /_  / / /
+ /_/  |_|\___/  \___/  \___/ /_/   \___/ /_/     \__,_/  \__/  /_/   \____/ /_/ /_/ 
+ 
+ 
+	By Aritz Beobide-Cardinal (ARitz Cracker) && James R Swift (James xX)
+ 
+--------------------------------------------------------------------------------------]]--
+
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 include( "shared.lua" )
-local fol = GM.FolderName.."/gamemode/includes/"
-
-local sharedfiles, _ = file.Find( fol.."shared/*.lua", "LUA" )
-local serverfiles, _ = file.Find( fol.."server/*.lua", "LUA" )
-local clientfiles, _ = file.Find( fol.."client/*.lua", "LUA" )
-	
-Car.Msg("####     Loading Serverside Files..     ####")
-for i, v in ipairs( serverfiles ) do
-	Car.Msg("#### Loading: "..fol.."server/"..v)
-	include( fol.."server/"..v )
-end
-for i, v in ipairs( sharedfiles ) do
-	Car.Msg("#### Loading: "..fol.."shared/"..v)
-	include( fol.."shared/"..v )
-end
-Car.Msg("####     Sending Clientside Files..     ####")
-for i, v in ipairs( clientfiles ) do
-	Car.Msg("#### Sending: "..fol.."client/"..v)
-	AddCSLuaFile( fol.."client/"..v )
-end
-for i, v in ipairs( sharedfiles ) do
-	Car.Msg("#### Sending: "..fol.."shared/"..v)
-	AddCSLuaFile( fol.."shared/"..v )
-end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,3 +1,15 @@
+--[[--------------------------------------------------------------------------------------
+ _______                     ______                      _____ _____                
+ ___    |___________________ ___  /_____ ______________ ___  /____(_)______ _______ 
+ __  /| |_  ___/_  ___/_  _ \__  / _  _ \__  ___/_  __ `/_  __/__  / _  __ \__  __ \
+ _  ___ |/ /__  / /__  /  __/_  /  /  __/_  /    / /_/ / / /_  _  /  / /_/ /_  / / /
+ /_/  |_|\___/  \___/  \___/ /_/   \___/ /_/     \__,_/  \__/  /_/   \____/ /_/ /_/ 
+ 
+ 
+	By Aritz Beobide-Cardinal (ARitz Cracker) && James R Swift (James xX)
+ 
+--------------------------------------------------------------------------------------]]--
+
 DeriveGamemode( "sandbox" )
 GM.Name = "Acceleration"
 GM.Author = "BlueStone Technological Enterprises Inc."
@@ -13,7 +25,7 @@ Car = Car or {}
 
 function GM:Initialize()
 
-end 
+end
 
 function GM:CreateTeams()
 	
@@ -31,14 +43,68 @@ function GM:CreateTeams()
 end
 
 function Car.Msg(msg)
+
 	Msg("Car: "..tostring(msg).."\n")
+	
 end
 
+function Car.IncludeFromDirectory( GM, directory )
 
-Car.Msg("Running...\n _______                     ______                      _____ _____                \n ___    |___________________ ___  /_____ ______________ ___  /____(_)______ _______ \n __  /| |_  ___/_  ___/_  _ \\__  / _  _ \\__  ___/_  __ `/_  __/__  / _  __ \\__  __ \\\n _  ___ |/ /__  / /__  /  __/_  /  /  __/_  /    / /_/ / / /_  _  /  / /_/ /_  / / /\n /_/  |_|\\___/  \\___/  \\___/ /_/   \\___/ /_/     \\__,_/  \\__/  /_/   \\____/ /_/ /_/ \n")
-Car.Msg(table.Random({"Go faster, baby!","James is my gay best friend","VROOM VROOM","Everything is not what it seems"}))
-Car.Msg("By Aritz Beobide-Cardinal (ARitz Cracker) && James R Swift (James xX)")
-Car.Update = GM.Update
-Car.Version = GM.Version
-Car.Msg("Version: "..Car.Version)
-Car.Msg("Updated on: "..Car.Update)
+	local folder = GM.FolderName.."/gamemode/"
+
+	for i, v in ipairs( file.Find( folder .. directory .. "/*.lua", "LUA" ) ) do
+		Car.Msg("#### Loading: "..folder.."/"..v)
+		include( folder .. directory .. "/" .. v )
+	end
+	
+end
+
+function Car.ClientsideFromDirectory( GM, directory )
+
+	local folder = GM.FolderName.."/gamemode/"
+
+	for i, v in ipairs( file.Find( folder .. directory .. "/*.lua", "LUA" ) ) do
+		Car.Msg("#### Sending: ".. folder .. directory .."/"..v)
+		AddCSLuaFile( folder .. directory .. "/" .. v )
+	end
+	
+end
+
+function Car.StartupMessage( GM )
+
+	Car.Msg("Running...\n _______                     ______                      _____ _____                \n ___    |___________________ ___  /_____ ______________ ___  /____(_)______ _______ \n __  /| |_  ___/_  ___/_  _ \\__  / _  _ \\__  ___/_  __ `/_  __/__  / _  __ \\__  __ \\\n _  ___ |/ /__  / /__  /  __/_  /  /  __/_  /    / /_/ / / /_  _  /  / /_/ /_  / / /\n /_/  |_|\\___/  \\___/  \\___/ /_/   \\___/ /_/     \\__,_/  \\__/  /_/   \\____/ /_/ /_/ \n")
+	Car.Msg(table.Random({"Go faster, baby!","James is my gay best friend","VROOM VROOM","Everything is not what it seems"}))
+	Car.Msg("By Aritz Beobide-Cardinal (ARitz Cracker) && James R Swift (James xX)")
+	Car.Update = GM.Update
+	Car.Version = GM.Version
+	Car.Msg("Version: "..Car.Version)
+	Car.Msg("Updated on: "..Car.Update)
+	
+end
+
+function Car.Initialize( )
+
+	Car.StartupMessage( GM )
+
+	if ( SERVER ) then
+		
+		Car.Msg("####     Loading Serverside Files..     ####")
+		Car.IncludeFromDirectory( GM, "includes/server" )
+		
+		Car.Msg("####     Sending Clientside Files..     ####")
+		Car.ClientsideFromDirectory( GM, "includes/client" )
+		Car.ClientsideFromDirectory( GM, "includes/shared" )
+		
+	else
+	
+		Car.Msg("####     Loading Clientside Files..     ####")
+		Car.IncludeFromDirectory( GM, "includes/client" )
+		
+	end
+	
+	Car.Msg("####     Loading Shared Files..     ####")
+	Car.IncludeFromDirectory( GM, "includes/shared" )
+	
+end
+
+Car.Initialize( )
